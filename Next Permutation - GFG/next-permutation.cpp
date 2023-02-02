9d2d295beffddebb90ee576b5502e3e1fd279af2
Aman kr singh <101496965+singhkumaraman@users.noreply.github.com>
2023-02-03 00:42:43 +0530

    Added solution - LeetHub

---
+++ Next Permutation - GFG/next-permutation.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+vector<int> nextPermutation(int N, vector<int> arr)
+{
+     int i, j;
+     int flag=0;
+     for (i = N - 1; i > 0; i--)
+     {
+          if (arr[i] > arr[i - 1])
+          {
+               i = i - 1;
+               flag=1;
+               break;
+          }
+     }
+     if (flag==0)
+     {
+          sort(arr.begin(), arr.end());
+          return arr;
+     }
+     for (j = N - 1; j >= 0; j--)
+     {
+          if (arr[j] > arr[i])
+          {
+               swap(arr[i], arr[j]);
+               break;
+          }
+     }
+     sort(arr.begin() + i + 1, arr.end());
+     return arr;
+}
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        vector<int> arr(N);
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        vector<int> ans = ob.nextPermutation(N, arr);
+        for(int u: ans)
+            cout<<u<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
